399b04416abfca0e3850a746eaff3b1248af04e6
Ivo <32672367+ivovandaalen@users.noreply.github.com>
2023-04-14 12:04:15 +0200

    Inventory work

---
+++ Assets/Scripts/Inventory/InventorySystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    //private Dictionary<InventoryItemData, InventoryItem> itemDictionary;
+    //public List<InventoryItem> inventory { get; private set; }
+
+    //private void Awake()
+    //{
+    //    inventory = new List<InventoryItem>();
+    //    itemDictionary = new Dictionary<InventoryItemData, InventoryItem>();
+    //}
+
+    //private void Update()
+    //{
+        
+    //}
+}

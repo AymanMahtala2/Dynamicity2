acc0a4a7000497e1679c66c35a8eea021f9ffaee
Ivo <32672367+ivovandaalen@users.noreply.github.com>
2023-04-19 11:22:53 +0200

    Added assets

--- Assets/Scripts/Inventory/ItemData.cs
+++ Assets/Scripts/Inventory/ItemData.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
 public class ItemData : ScriptableObject
 {
-    public string id;
     public string displayName;
     public Sprite icon;
     //public GameObject prefab;
